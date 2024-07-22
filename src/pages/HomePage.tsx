785f52023af264d9234c1eed5caba2f63f5b3656
moon <140053617+Moonbyeol@users.noreply.github.com>
2024-07-22 21:56:28 +0900

    docker

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,18 @@
+import React from 'react';
 import Header from '@/components/common/header';
 import Features from '@/components/home/features';
 import Hero from '@/components/home/hero';
 import Newsletter from '@/components/home/newsletter';
 import Zigzag from '@/components/home/zigzag';
 
-const HomePage = ({ isLoggedIn, hasToken, handleLogout }) => {
+// 인터페이스 정의
+interface HomePageProps {
+  isLoggedIn: boolean;
+  hasToken: boolean;
+  handleLogout: () => void;
+}
+
+const HomePage: React.FC<HomePageProps> = ({ isLoggedIn, hasToken, handleLogout }) => {
   return (
     <>
       <Header isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
